08bb444bcd39b64a6c769102395a2f3d343b0ad9
Zachariah Sharma <zachariah.sharma@icloud.com>
2020-11-04 15:51:53 -0500

    asdf

---
+++ dynamic-routes-step-1/pages/404.js
@@ -0,0 +1,3 @@
+export default function Custom404() {
+    return <h1>Rahul is a chicken head</h1>
+}
--- dynamic-routes-step-1/pages/posts/[id].js
+++ dynamic-routes-step-1/pages/posts/[id].js
@@ -1,14 +1,27 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
+
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+  <title>{postData.title}</title>
+      </Head>
+      <article>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      
+      </article>
     </Layout>
   )
 }
@@ -22,7 +35,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
